7e22f6049afeb0b99bf8415f3fc5c852fcecc3f3
Victor <ivchenko.victor.04@gmail.com>
2024-11-28 21:18:52 +0300

    Обновление шаблона одиночной услуги: добавление меток, превью изображения и улучшение стилей для адаптивности

--- theme/single-service.php
+++ theme/single-service.php
@@ -5,43 +5,40 @@
     <?php if (have_posts()):
       while (have_posts()):
         the_post(); ?>
-        <div id="service-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <!-- Заголовок -->
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <!-- Заголовок и метки -->
           <div class="service-header">
             <h1 class="service-title"><?php the_title(); ?></h1>
-            <div class="service-meta">
-              <span class="service-date">Опубликовано: <?php echo get_the_date(); ?></span>
-              <?php if (has_category()): ?>
-                <span class="service-category">Категория: <?php the_category(', '); ?></span>
-              <?php endif; ?>
+            <div class="service-tags">
+              <?php
+              $tags = get_the_tags();
+              if ($tags) {
+                foreach ($tags as $tag) {
+                  echo '<span class="service-tag">' . esc_html($tag->name) . '</span>';
+                }
+              }
+              ?>
             </div>
           </div>
+
+          <!-- Превью изображения -->
+          <?php if (has_post_thumbnail()): ?>
+            <div class="service-thumbnail">
+              <?php the_post_thumbnail('large', ['class' => 'service-image']); ?>
+            </div>
+          <?php endif; ?>
 
           <!-- Цена -->
           <div class="service-price">
             <?php
             $price = get_post_meta(get_the_ID(), 'price', true);
             if ($price) {
-              echo '<p><strong>Цена услуги:</strong> ' . esc_html($price) . ' руб.</p>';
+              echo '<span>Цена: от ' . esc_html($price) . ' ₽</span>';
             }
             ?>
           </div>
 
-          <!-- Метки -->
-          <div class="service-tags">
-            <?php
-            $tags = get_the_tags(get_the_ID());
-            if ($tags) {
-              echo '<ul class="tags">';
-              foreach ($tags as $tag) {
-                echo '<li class="tag"><span>' . esc_html($tag->name) . '</span></li>';
-              }
-              echo '</ul>';
-            }
-            ?>
-          </div>
-
-          <!-- Содержимое -->
+          <!-- Описание -->
           <div class="service-content">
             <?php the_content(); ?>
           </div>
